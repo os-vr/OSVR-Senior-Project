33e40ed61d8bcb91231c28285f223998efa0213e
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2018-11-28 14:15:34 -0500

    Project setup for Gesturing Framework

---
+++ Gesturing/Assets/CircularQueue.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularQueue<T> : IEnumerable<T> {
+
+    private int maxSize;
+    private LinkedList<T> data;
+
+    public CircularQueue(int maxSize){
+        this.data = new LinkedList<T>();
+        this.maxSize = maxSize;
+    }
+
+    public void Enqueue(T t)
+    {
+        data.AddFirst(t);
+        if(data.Count > maxSize)
+        {
+            data.RemoveLast();
+        }
+    }
+
+    public void Dequeue()
+    {
+        data.RemoveLast();
+    }
+
+    public void Clear()
+    {
+        data.Clear();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach(T t in data)
+        {
+            yield return t;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public T[] ToArray()
+    {
+        T[] ret = new T[maxSize];
+        data.CopyTo(ret, 0);
+        return ret;
+    }
+
+}
---
+++ Gesturing/Assets/PathRenderer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathRenderer : MonoBehaviour {
+
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 200;
+    public CircularQueue<Vector3> queue;
+
+
+    void Start()
+    {
+        queue = new CircularQueue<Vector3>(lengthOfLineRenderer);
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = lengthOfLineRenderer;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1, 0.0f), new GradientAlphaKey(1, 1.0f) }
+            );
+        lineRenderer.colorGradient = gradient;
+    }
+
+    void Update()
+    {
+
+        Vector3 newPos = Input.mousePosition;
+        newPos = Camera.main.ScreenToWorldPoint(new Vector3(newPos.x, newPos.y, 10));
+        queue.Enqueue(newPos);
+
+
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        var points = new Vector3[lengthOfLineRenderer];
+
+        lineRenderer.SetPositions(queue.ToArray());
+    }
+}
