64eed5ce37cb6a2551e26207da1c44a77690704c
Charlie Hershberger <hershbca@hershbca-w541.rose-hulman.edu>
2018-12-03 16:52:24 -0500

    Here are the scaling and rotating scripts

---
+++ UI/UI-Charlie/RotateToUser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToUser : MonoBehaviour {
+    public GameObject Camera;
+    public GameObject PivotPoint;
+    public float distanceFromObject;
+    Vector3 LastUp = new Vector3(0,1,0);
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        Vector3 direction = PivotPoint.transform.position - Camera.transform.position;
+        Vector3 up = Camera.transform.up;
+        up = (up - Vector3.Project(up, direction)).normalized;
+        if (up.magnitude != 0)
+        {
+            LastUp = up;
+        }
+        else
+        {
+            up = LastUp;
+        }
+        Quaternion Look = Quaternion.LookRotation(direction, Camera.transform.up);
+        transform.rotation = Look;
+        transform.position = PivotPoint.transform.position - (distanceFromObject * direction.normalized);
+    }
+}
---
+++ UI/UI-Charlie/ScaleToUser.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleToUser : MonoBehaviour {
+    public GameObject Camera;
+    public float Scale = 1;
+    public float SizeAtOneMeter;
+    public float DisapearAtCloseDistance = -1;
+    public bool DisapearWhenFarAway = false;
+    public float DisapearAtFarDistance;
+    void Start () {
+        if (Scale > 1000000)
+        {
+            Debug.LogError("Scale is increadibly large, please check the function that supplies this number");
+        }
+        if (Scale < 0)
+        {
+            Debug.LogError("Scale is negative, please use a positve number");
+        }
+        Scale = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = (Camera.transform.position - this.transform.position).magnitude;
+        if (distance < DisapearAtCloseDistance)
+        {
+            this.GetComponent<Renderer>().enabled = false;
+            foreach (Renderer r in this.GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = false;
+            }
+        }else if (DisapearWhenFarAway)
+        {
+            if (DisapearAtFarDistance < distance)
+            {
+                this.GetComponent<Renderer>().enabled = false;
+                foreach (Renderer r in this.GetComponentsInChildren<Renderer>())
+                {
+                    r.enabled = false;
+                }
+            }
+        }
+        else
+        {
+            this.GetComponent<Renderer>().enabled = true;
+            foreach (Renderer r in this.GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = true;
+            }
+        }
+        //print(this.transform.lossyScale);
+        //this.transform.localScale = (distance) * this.transform.lossyScale;
+    }
+}
