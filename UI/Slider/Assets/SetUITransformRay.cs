9af376d231aa7203b35df2270c75577719f5d835
Abhimanyu Yadav <yadava@YADAVA-W541.rose-hulman.edu>
2018-12-11 16:43:24 -0500

    Slider code added

---
+++ UI/Slider/Assets/SetUITransformRay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetUITransformRay : MonoBehaviour
+{
+    [SerializeField]
+    private OVRInputModule inputModule;
+    [SerializeField]
+    private OVRGazePointer gazePointer;
+
+    public void SetUIRays()
+    {
+        inputModule.rayTransform = ControllerInfo.CONTROLLER_DATA_FOR_RAYS.transform;
+        gazePointer.rayTransform = ControllerInfo.CONTROLLER_DATA_FOR_RAYS.transform;
+    }
+}
