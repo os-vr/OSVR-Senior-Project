cb84a75066906bc18c895f6cfaff4e79777369d2
Chris Nurrenberg <nurrencd@rose-hulman.edu>
2018-12-14 21:30:35 -0500

    staging more new files for the continuous update

---
+++ Gesturing/Assets/Gestures/OverheadSwipeGesture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadSwipeGesture : Gesture{
+
+    public OverheadSwipeGesture(Vector3 start, Vector3 end, float radius, Normalizer n) : base(n)
+    {
+        List<Check> startChecks = new List<Check>();
+        startChecks.Add(new RadiusCheck(start, radius));
+        //TODO: generalize this for the two passed in points
+
+        List<Check> endChecks = new List<Check>();
+        PrismCheck p = new PrismCheck(new Vector3(0, 0, 0), new Vector3(1, .85f, .5f));
+        p.passValue = GSuccess.HALT;
+        p.failValue = GSuccess.PASS;
+        endChecks.Add(p);
+        endChecks.Add(new RadiusCheck(end, radius));
+
+        Stage s = new Stage(startChecks);
+        Stage e = new Stage(endChecks);
+        AddStage(s);
+        //TODO: add exclusion zone
+        AddStage(e);
+
+    }
+}
---
+++ Gesturing/Assets/PrismCheck.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrismCheck : Check {
+
+    private Vector3 position1;
+    private Vector3 position2;
+    private BoxCollider boxCollider;
+    private GameObject box;
+    public GSuccess passValue;
+    public GSuccess failValue;
+
+    public PrismCheck(Vector3 pos1, Vector3 pos2)
+    {
+        this.position1 = pos1;
+        this.position2 = pos2;
+
+        this.VisualizeCheck();
+
+        
+    }
+
+    public GSuccess CheckPoint(GTransform gTransform)
+    {
+        if (boxCollider.bounds.Contains(gTransform.position))
+        {
+            box.GetComponent<Renderer>().material.SetColor("_WireColor", Color.red);
+            Debug.Log("Entered the cube");
+            return passValue;
+        }
+        box.GetComponent<Renderer>().material.SetColor("_WireColor", Color.blue);
+
+        return failValue;
+    }
+
+
+    private void VisualizeCheck()
+    {
+        box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        boxCollider = box.GetComponent<BoxCollider>();
+
+        box.GetComponent<Renderer>().material = new Material(Shader.Find("WireframeShader"));
+        box.transform.position = (position1 + position2)/2;
+        box.transform.localScale = position2 - position1;
+
+
+
+    }
+}
