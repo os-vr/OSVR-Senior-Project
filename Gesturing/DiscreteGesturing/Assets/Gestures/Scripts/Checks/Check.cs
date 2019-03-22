08f47da9cd5bc980511ffc03307742df76b2f2a7
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-03-22 15:51:47 -0400

    Documentation of Check and GestureEvent classes

--- Gesturing/DiscreteGesturing/Assets/Gestures/Scripts/Checks/Check.cs
+++ Gesturing/DiscreteGesturing/Assets/Gestures/Scripts/Checks/Check.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 namespace Gestures {
 
     /// <summary>
-    /// An interface representing a single piece of a gesture. 
+    /// An interface representing a single segment of a gesture. 
     /// </summary>
+    /// <remarks>
+    /// Current implementations include Lines, Arcs, and Radii, all which deal with the position element of a GTransform
+    /// </remarks>
     public abstract class Check {
         public abstract float CheckPasses(GTransform transform);
         public virtual void VisualizeCheck(Rect grid) {}
--- Gesturing/DiscreteGesturing/Assets/Gestures/Scripts/GestureEvent.cs
+++ Gesturing/DiscreteGesturing/Assets/Gestures/Scripts/GestureEvent.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.Events;
 
 namespace Gestures {
+    /// <summary>
+    /// Wrapper class for a Unity Event that takes in GestureMetaData as a parameter
+    /// </summary>
     public class GestureEvent : UnityEvent<GestureMetaData> {
         public GestureEvent() : base() {}
         public GestureEvent(UnityAction<GestureMetaData> action) : base() {
