a1b9ca71de2474641e961461aca5f63e2d196007
Jacob Gathof <gathofjd@rose-hulman.edu>
2019-04-01 17:45:28 -0400

    Cleanup master

--- Gesturing/ContinuousGesturing/Assets/Checks/Check.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Check {
-    GStatus CheckPoint(GTransform transform);
-
-    GStatus CheckAll(List<GTransform> buffer);
-}
