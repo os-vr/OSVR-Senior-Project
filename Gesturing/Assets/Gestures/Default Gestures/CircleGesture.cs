5569ee3a8575816ede412a9407dd7bf517b12f4d
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2019-04-21 18:56:27 -0400

    Circle Gesture and TouchController Comments

--- Gesturing/Assets/Gestures/Default Gestures/CircleGesture.cs
+++ Gesturing/Assets/Gestures/Default Gestures/CircleGesture.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 namespace Gestures {
     /// <summary>
-    /// A circle gesture set with a FittedNormalizer.
+    /// A Circle gesture set with a FittedNormalizer.
     /// </summary>
     public class CircleGesture : Gesture {
-        public CircleGesture() : base() {
+
+        /// <summary>
+        /// Create a Circle Gesture with a specified tolerance.
+        /// </summary>
+        /// <param name="tolerance">The tolerance value for the circle gesture. Default is 0.4f</param>
+        public CircleGesture(float tolerance = 0.4f) : base() {
             this.AddOnceChecks(new List<Check> {
-                new ArcCheck(new Vector3(0, 1, 0), 90, new Vector3(0,0,0)),
-                new ArcCheck(new Vector3(1, 0, 0), 90, new Vector3(0,0,0)),
-                new ArcCheck(new Vector3(0, -1, 0), 90, new Vector3(0,0,0)),
-                new ArcCheck(new Vector3(-1, 0, 0), 90, new Vector3(0,0,0)),
+                new ArcCheck(new Vector3(0, 1, 0), 90, new Vector3(0,0,0), tolerance),
+                new ArcCheck(new Vector3(1, 0, 0), 90, new Vector3(0,0,0), tolerance),
+                new ArcCheck(new Vector3(0, -1, 0), 90, new Vector3(0,0,0), tolerance),
+                new ArcCheck(new Vector3(-1, 0, 0), 90, new Vector3(0,0,0), tolerance),
             })
-            .SetNormalizer(new FittedNormalizer(new Vector3(-1, -1, 0), new Vector3(1, 1, 0)));
+            .SetNormalizer(new FittedNormalizer(new Vector3(-1, -1, 0), new Vector3(1, 1, 0), true));
 
         }
     }
