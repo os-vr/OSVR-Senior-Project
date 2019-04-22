f9afe5f8bb8e9ea9cc037d11c054ea28e90ab898
Jacob Gathof <gathofjd@rose-hulman.edu>
2019-04-21 20:10:50 -0400

    Documenting Checks

--- Gesturing/Assets/Gestures/Scripts/Checks/LineCheck.cs
+++ Gesturing/Assets/Gestures/Scripts/Checks/LineCheck.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 namespace Gestures {
     /// <summary>
-    /// A Line check to check if 
+    /// A Check to determine whether a point fits inside a line segment.
     /// </summary>
     public class LineCheck : Check {
 
-        public Vector3 firstPosition;
-        public Vector3 secondPosition;
-        public float precision;
+        private Vector3 firstPosition;
+        private Vector3 secondPosition;
+        private float precision;
         private LineRenderer lineRenderer;
+
         /// <summary>
-        /// 
+        /// Create a Line Check used to determine whether a point fits inside the defined line segment. 
         /// </summary>
-        /// <param name="firstPosition">The first point of the line.</param>
-        /// <param name="secondPosition">The second point of the line.</param>
-        /// <param name="precision">Distance from the line considered to be on the line.</param>
-        public LineCheck(Vector3 firstPosition, Vector3 secondPosition, float precision = 0.4f) {
+        /// <param name="firstPosition">The first point of the line segment.</param>
+        /// <param name="secondPosition">The second point of the line segment.</param>
+        /// <param name="tolerance">Distance from the line considered to be on the line.</param>
+        public LineCheck(Vector3 firstPosition, Vector3 secondPosition, float tolerance = 0.4f) {
             this.firstPosition = firstPosition;
             this.secondPosition = secondPosition;
-            this.precision = precision;
+            this.precision = tolerance;
         }
 
-        override public float CheckPasses(GTransform g) {
 
+        /// <summary>
+        /// Determine whether a single GTransform fits inside the area defined by the line segment 
+        /// </summary>
+        /// <param name="g">GTransform to check position against</param>
+        /// <returns> Returns a float (between 0 and 1) representing the distance from the center of the line, or -1 if the check fails </returns>
+        override public float CheckPasses(GTransform g) {
             float distance = Vector3.Distance(g.position, GetClosestPointOnLineSegment(firstPosition, secondPosition, g.position));
             if (distance > precision/2.0f) {
                 return -1;
@@ -35,7 +41,7 @@ namespace Gestures {
         }
 
 
-        public Vector3 GetClosestPointOnLineSegment(Vector3 A, Vector3 B, Vector3 P) {
+        private Vector3 GetClosestPointOnLineSegment(Vector3 A, Vector3 B, Vector3 P) {
             Vector3 AP = P - A;
             Vector3 AB = B - A;
 
